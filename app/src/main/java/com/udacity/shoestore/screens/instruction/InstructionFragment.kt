a0d81dd309300fe8f1581dd1ccef2623b4f07335
IronManYG <hussain.yg2010@gmail.com>
2020-12-31 10:18:50 +0300

    Update Fragments

--- app/src/main/java/com/udacity/shoestore/screens/instruction/InstructionFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/instruction/InstructionFragment.kt
@@ -1,4 +1,25 @@
 package com.udacity.shoestore.screens.instruction
 
-class InstructionFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.InstructionFragmentBinding
+
+class InstructionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        val binding: InstructionFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.instruction_fragment, container, false)
+        return binding.root
+    }
+
 }
