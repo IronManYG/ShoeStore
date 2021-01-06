cf55b68c600c1af209ca8f8dadc3ec490712be66
IronManYG <hussain.yg2010@gmail.com>
2021-01-07 00:11:16 +0300

    add buttons

--- app/src/main/java/com/udacity/shoestore/screens/shoeDetail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoeDetail/ShoeDetailFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.ShoeDetailBinding
 
 class ShoeDetailFragment : Fragment() {
 
@@ -14,7 +17,22 @@ class ShoeDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shoe_detail, container, false)
+        val binding: ShoeDetailBinding = DataBindingUtil.inflate(
+            inflater, R.layout.shoe_detail, container, false)
+
+        binding.cancelButton.setOnClickListener{
+            findNavController().navigate(ShoeDetailFragmentDirections.actionShoeDetailToShoesListFragment())
+        }
+
+        binding.saveButton.setOnClickListener{
+            findNavController().navigate(ShoeDetailFragmentDirections.actionShoeDetailToShoesListFragment())
+        }
+
+        return binding.root
+    }
+
+    fun addShoe(){
+
     }
 
 }
