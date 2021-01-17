ec5dfe652d5707d07593df199109605446531c8a
IronManYG <hussain.yg2010@gmail.com>
2021-01-17 22:58:25 +0300

    Update to handle empty fields & show massage if that happens

--- app/src/main/java/com/udacity/shoestore/models/ShoeViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/models/ShoeViewModel.kt
@@ -5,17 +5,31 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
+import com.udacity.shoestore.Event
 import java.util.ArrayList
 
 class ShoeViewModel : ViewModel() {
+
+    var isShoeEmpty: Boolean = false
 
     private val _shoesList = MutableLiveData<ArrayList<Shoe>>()
     val shoesList: LiveData<ArrayList<Shoe>>
         get() = _shoesList
 
+    private val statusMessage = MutableLiveData<Event<String>>()
+    val message : LiveData<Event<String>>
+        get() = statusMessage
+
     fun saveButtonClick(view: View, shoe: Shoe){
-        _shoesList.value?.add(shoe)
-        view.findNavController().navigateUp()
+        isShoeEmpty = if(shoe.name != "" && shoe.company != "" && shoe.size != 0.0 && shoe.description != "") {
+            _shoesList.value?.add(shoe)
+            view.findNavController().navigateUp()
+            true
+        }
+        else{
+            statusMessage.value = Event("Please fill in all the fields.")
+            false
+        }
     }
 
     init {
--- app/src/main/java/com/udacity/shoestore/screens/shoeDetail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoeDetail/ShoeDetailFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
@@ -38,6 +40,12 @@ class ShoeDetailFragment : Fragment() {
         binding.shoe = Shoe("", 0.0, "", "",images = mutableListOf())
 
         binding.lifecycleOwner = this
+
+        viewModel.message.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let {
+                Toast.makeText(requireActivity(), it, Toast.LENGTH_LONG).show()
+            }
+        })
 
         binding.cancelButton.setOnClickListener{
             findNavController().navigate(ShoeDetailFragmentDirections.actionShoeDetailToShoesListFragment())
